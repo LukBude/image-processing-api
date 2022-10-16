5718f0dfd90b2d811cfbe6c8a0ee90197c2405ad
lukb <lukas.buderath@gmail.com>
2022-10-16 20:12:20 +0200

    adds middleware.

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,10 +1,22 @@
 import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 import routes from './route/routes';
 import { HttpStatusCode } from './error/HttpStatusCode';
 import { ApiError } from './error/ApiError';
 
 const server = express();
 const port = 3000;
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+});
+
+server.use(morgan('common'));
+server.use(helmet());
+server.use(limiter);
 
 server.use('/api', routes);
 
